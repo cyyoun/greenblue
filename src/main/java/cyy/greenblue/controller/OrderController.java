4261f608ad1f4b5c790c5242136ed767a1d87ada
cyyoun <cyyoun41@gmail.com>
2023-11-26 18:23:27 +0900

    Refactor: OrderController 구매확정,취소 소스보완

--- src/main/java/cyy/greenblue/controller/OrderController.java
+++ src/main/java/cyy/greenblue/controller/OrderController.java
@@ -1,7 +1,6 @@
 package cyy.greenblue.controller;
 
 import cyy.greenblue.domain.OrderProduct;
-import cyy.greenblue.domain.OrderSheet;
 import cyy.greenblue.dto.OrderProductDto;
 import cyy.greenblue.dto.OrderRequestDto;
 import cyy.greenblue.service.OrderProductService;
@@ -20,7 +19,6 @@ import java.util.stream.Collectors;
 public class OrderController {
 
     private final OrderProductService orderProductService;
-    private final OrderSheetService orderSheetService;
 
     private List<OrderProductDto> changeDto(List<OrderProduct> orderProducts) {
         return orderProducts.stream()
@@ -31,7 +29,6 @@ public class OrderController {
                         orderProduct.getProduct().getId(),
                         orderProduct.getOrderSheet().getId()))
                 .collect(Collectors.toList());
-
     }
 
     @PostMapping
@@ -44,22 +41,31 @@ public class OrderController {
             List<OrderProductDto> orderProductDtos = changeDto(savedOrderProducts);
             return ResponseEntity.status(HttpStatus.OK).body(orderProductDtos);
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("주문 실패");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("주문에 실패하였습니다.");
     }
 
     @PostMapping("/{orderSheetId}/cancel") //주문 취소
-    public ResponseEntity<Object> cancel(@PathVariable long orderSheetId) {
-        OrderSheet orderSheet = orderSheetService.cancel(orderSheetId);
-        orderProductService.cancel(orderSheet);
-        return ResponseEntity.status(HttpStatus.OK).body("취소 성공");
+    public ResponseEntity<Object> allCancel(@PathVariable long orderSheetId) {
+        orderProductService.allCancel(orderSheetId);
+        return ResponseEntity.status(HttpStatus.OK).body("주문을 취소하였습니다.");
+    }
+
+    @PostMapping("/{orderProductId}/cancel")
+    public ResponseEntity<Object> oneCancel(@PathVariable long orderProductId) {
+        orderProductService.oneCancel(orderProductId);
+        return ResponseEntity.status(HttpStatus.OK).body("주문을 취소하였습니다.");
+    }
+    @PostMapping("/{orderSheetId}/confirm") //구매 상품 전부 구매 확정
+    public ResponseEntity<Object> allPurchaseConfirm(@PathVariable long orderSheetId) {
+        orderProductService.allPurchaseConfirm(orderSheetId);
+        return ResponseEntity.status(HttpStatus.OK).body("구매확정 되었습니다.");
     }
 
     @PostMapping("/{orderProductId}/confirm") //구매 확정
-    public String updateMemberPurchaseStatus(@PathVariable long orderProductId) {
-        orderProductService.updateMemberPurchaseStatus(orderProductId);
-        return "구매 확정되었습니다.";
+    public ResponseEntity<Object> onePurchaseConfirm(@PathVariable long orderProductId) {
+        orderProductService.onePurchaseConfirm(orderProductId);
+        return ResponseEntity.status(HttpStatus.OK).body("구매확정 되었습니다.");
     }
-
 
     @GetMapping("/member/{memberId}")
     public List<OrderProductDto> list(@PathVariable long memberId) {
